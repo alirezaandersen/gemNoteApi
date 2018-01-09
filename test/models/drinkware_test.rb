09a8c8c5656afe8d6937e0eb7879c3cc1b6dadc2
Alireza Andersen <alireza.andersen@gmail.com>
2018-01-09 00:03:52 -0700

    added rspec

--- test/models/drinkware_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DrinkwareTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/food_beverage_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FoodBeverageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
